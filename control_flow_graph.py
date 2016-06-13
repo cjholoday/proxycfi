e498bbae6aa167a92c8787c259fbc774fd7ffc16
Colton Holoday <choloday@umich.edu>
2016-06-13 14:45:03 -0400

    Fix cfg.py syntax. Add iteration for cfg

--- control_flow_graph.py
+++ control_flow_graph.py
@@ -11,10 +11,23 @@ class ControlFlowGraph:
     def funct(self, funct_name):
         return self._funct_edges[funct_name]
 
+    def __iter__(self):
+        return iter(ControlFlowGraphIterator(self))
+
+class ControlFlowGraphIterator:
+    def __init__(self, cfg):
+        self.cfg_iter = cfg._funct_edges.iteritems()
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.cfg_iter.next()[1]
+
 class Function:
     def __init__(self, name, sites, return_set):
-        assert name is types.StringType
-        assert return_set is TupleType
+        assert type(name) is types.StringType
+        assert type(return_set) is types.ListType
 
         # all functions that can be returned to in the format:
         # (function_label, num_times_function_calls_this_one)
@@ -24,8 +37,8 @@ class Function:
 
 class Site:
     def __init__(self, line_num, targets):
-        assert line_num is types.IntType
-        assert targets is types.ListType
+        assert type(line_num) is types.IntType
+        assert type(targets) is types.ListType
 
         self.line_num = line_num
         self.targets = targets
