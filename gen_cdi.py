36adcd4013f8ab7f63646a7c7792870070f0bf9d
Colton Holoday <choloday@umich.edu>
2016-06-14 14:10:35 -0400

    Add json serialization

--- gen_cdi.py
+++ gen_cdi.py
@@ -1,11 +1,17 @@
 import sys
 import asm_parsing
+import jsonpickle
 from gen_cfg import gen_cfg
 from gen_cdi_asm import gen_cdi_asm
 
 ############################
 # Script
 ############################
+
+def print_cfg_as_json(cfg):
+    with open('cdi_cfg.json', 'w') as cfg_file:
+        encoding = jsonpickle.encode(cfg)
+        cfg_file.write(encoding)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -17,4 +23,6 @@ if __name__ == "__main__":
         asm_file_descrs.append(asm_parsing.AsmFileDescription(filename))
 
     cfg = gen_cfg(asm_file_descrs)
+    print_cfg_as_json(cfg)
+
     gen_cdi_asm(cfg, asm_file_descrs)
