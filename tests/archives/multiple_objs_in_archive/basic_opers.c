2ebfbdbc5464c2b507abe807622e918392355fd4
Colton Holoday <choloday@umich.edu>
2017-05-11 15:53:11 -0400

    Add generic test for archives which has more than one object per archive

---
+++ tests/archives/multiple_objs_in_archive/basic_opers.c
@@ -0,0 +1,15 @@
+int add(int x, int y) {
+    return x + y;
+}
+
+int subtract(int x, int y) {
+    return x - y;
+}
+
+int multiply(int x, int y) {
+    return x * y;
+}
+
+int divide(int x, int y) {
+    return x / y;
+}
---
+++ tests/archives/multiple_objs_in_archive/formulas.c
@@ -0,0 +1,11 @@
+int area_triangle(int b, int h) {
+    return b * h / 2;
+}
+
+int area_square(int s) {
+    return s * s;
+}
+
+int area_circle(int r) {
+    return 3.14 * r * r;
+}
