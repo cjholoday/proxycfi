0604fb97c4045130be6ecae5b8dc716a64fd6184
Colton Holoday <choloday@umich.edu>
2017-05-15 16:18:40 -0400

    Print name of test while running all tests

--- tests/archives/run_tests.bash
+++ tests/archives/run_tests.bash
@@ -4,6 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$SCRIPT_DIR"
 for test_dir in $(ls | grep -v run_tests.bash); do
+    echo "running test '$test_dir'..."
     cd "$test_dir"
     ./run_test.bash > log.txt 2>&1
     if [ "$?" != 0 ]; then
