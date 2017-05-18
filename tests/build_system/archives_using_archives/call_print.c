1b950395b27eb4625bb41e3f4a6d99d8a195f78d
Colton Holoday <choloday@umich.edu>
2017-05-18 16:56:44 -0400

    Update archive test to include fptr call.

--- tests/build_system/archives_using_archives/call_print.c
+++ tests/build_system/archives_using_archives/call_print.c
@@ -1,3 +1,6 @@
+extern void print(char *str);
+
 void call_print(char *str) {
-    print(str);
+    void (*oper)(char *) = print;
+    oper(str);
 }
--- tests/build_system/archives_using_archives/test_creation_tools/print.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void print(char *str) {
-    printf("%s", str);
-}
