0ba20c862e3c79e4d62ea9450fc36b8f7d26fdb3
Colton Holoday <choloday@umich.edu>
2017-05-18 12:20:49 -0400

    Fix setting of wrappers to cdi

--- tools/set_wrappers_cdi.bash
+++ tools/set_wrappers_cdi.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-sudo ln -sf "$(pwd)/../../gcc_wrappers/cdi-as.py" /usr/local/cdi/cdi-as     
-sudo ln -sf "$(pwd)/../../gcc_wrappers/cdi-ld.py" /usr/local/cdi/cdi-ld     
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sudo rm /usr/local/cdi/cdi-as
+sudo rm /usr/local/cdi/cdi-ld
+sudo ln -sf "$SCRIPT_DIR/../gcc_wrappers/cdi-as.py" /usr/local/cdi/cdi-as     
+sudo ln -sf "$SCRIPT_DIR/../gcc_wrappers/cdi-ld.py" /usr/local/cdi/cdi-ld     
 
