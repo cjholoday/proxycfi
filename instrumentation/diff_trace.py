8b21d59637693fa36be9794549f28fe45aba1040
Colton Holoday <choloday@umich.edu>
2017-05-30 14:39:01 -0400

    Use names not addresses for input since addresses depend on execs

--- instrumentation/diff_trace.py
+++ instrumentation/diff_trace.py
@@ -3,32 +3,32 @@
 import sys
 import itertools
 from eprint import eprint
-from addr_translation import funct_name
+
+
 
 executable_fname = sys.argv[1]
 trace1 = open(sys.argv[2], 'r')
-trace_table1 = open(sys.argv[3], 'r')
+funct_table1 = open(sys.argv[3], 'r')
 
 trace2 = open(sys.argv[4], 'r')
-trace_table2 = open(sys.argv[5], 'r')
+funct_table2 = open(sys.argv[5], 'r')
 
 trace1_id_to_name = [None]
-for line in trace_table1:
-    funct_addr = line.split(' ')[1]
-    trace1_funct_name = funct_name(executable_fname, funct_addr)
-    trace1_id_to_name.append(trace1_funct_name)
+for line in funct_table1:
+    funct_name = line.split(' ')[1]
+    trace1_id_to_name.append(funct_name)
 
 # this dict allows us to build a translation between trace1 id's and trace2 id's
 trace2_name_to_id = dict()
-trace2_id_to_name = [None]
-for i, line in enumerate(trace_table2):
-    funct_addr = line.split(' ')[1]
-    trace2_funct_name = funct_name(executable_fname, funct_addr)
-    trace2_name_to_id[trace2_funct_name] = i + 1
-    trace2_id_to_name.append(trace2_funct_name)
 
-trace_table1.close()
-trace_table2.close()
+trace2_id_to_name = [None]
+for i, line in enumerate(funct_table2):
+    funct_name = line.split(' ')[1]
+    trace2_name_to_id[funct_name] = i + 1
+    trace2_id_to_name.append(funct_name)
+
+funct_table1.close()
+funct_table2.close()
 
 if len(trace1_id_to_name) != len(trace2_id_to_name):
     eprint('error: trace 1 and trace 2 have a different number of defined functions')
@@ -67,18 +67,22 @@ for line1, line2 in itertools.izip(trace1, trace2):
         if called_function == None:
             eprint('trace1 returns')
         else:
-            eprint('trace1 calls {}'.format())
+            eprint('trace1 calls {}'.format(called_function))
 
         called_function = trace2_id_to_name[funct_id2]
         if called_function == None:
             eprint('trace2 returns')
         else:
-            eprint('trace2 calls {}'.format(trace2_id_to_name[funct_id2]))
+            eprint('trace2 calls {}'.format(called_function))
 
         sys.exit(1)
 
     if funct_id2 == 0:
-        del backtrace[-1]
+        try:
+            del backtrace[-1]
+        except:
+            eprint('Too many returns in trace!')
+            sys.exit(1)
     else:
         backtrace.append(trace1_id_to_name[funct_id1])
 
