ed38120b006a9f9fd8c05da62d249656f7920dca
Colton Holoday <choloday@umich.edu>
2017-06-02 10:57:28 -0400

    Add correct output for callback test

--- tests/unsafe_shared_libs/callback/run_test.bash
+++ tests/unsafe_shared_libs/callback/run_test.bash
@@ -4,7 +4,7 @@ rm -f *.o *.s *.json *.i *.ftypes *.fptypes output out *.so
   
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
 
-# purpose: generate unsafe (non-CDI) shared library and link with it
+# purpose: generate an unsafe (non-CDI) shared library and link with it
 
 cdi_flags="-g --save-temps -fno-jump-tables"
 tools_dir="$SCRIPT_DIR/../../../tools"
@@ -13,7 +13,7 @@ gen_sl="$tools_dir/gen_sl.bash"
 "$gen_sl" libcaller.so caller.c
 
 cdi-gcc $cdi_flags main.c -L. libcaller.so -o out \
-    -Wl,-rpath="$SCRIPT_DIR"
+    -Wl,-rpath="$SCRIPT_DIR" 
 
 if [ "$?" != 0 ]; then
     echo ERROR: Compilation failed!
