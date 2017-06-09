c6b2e706e99fe72697a76c08e49ac0ef53cf2563
Colton Holoday <choloday@umich.edu>
2017-06-09 16:28:48 -0400

    New cdi_abort.cdi.s. This will break everything temporarily

--- converter/cdi_abort.cdi.s
+++ converter/cdi_abort.cdi.s
@@ -9,11 +9,11 @@
         .set .debug_msg_len, .-.debug_msg
 .newline_char:
         .string "\n"
+
 # parameters:
-#   %rdx: length of message passed 
-#   %rsi: pointer to string that will be printed
-#
-#   if %rdx contains 0, then only the generic string will be printed
+#   %rsi: pointer to a an unsigned designating the following string's length
+# 
+#   TODO: make this position independent
 _CDI_abort:
 .LFB0:
 	.cfi_startproc
@@ -22,17 +22,12 @@ _CDI_abort:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-        pushq   %rsi
-        pushq   %rdx
+        pushq   %rsi                        # save specific sled info for later
         movq    $.generic_msg, %rsi
         movq    $.generic_msg_len, %rdx
         movq    $1, %rax                    # write
         movq    $2, %rdi                    # to stderr
         syscall                             # print generic message
-        popq    %rdx
-        cmpq    $0, %rdx
-        je .abort
-        pushq   %rdx
         movq    $1, %rax
         movq    $2, %rdi
         movq    $.debug_msg_len, %rdx
@@ -40,8 +35,9 @@ _CDI_abort:
         syscall                             # print debug msg
         movq    $1, %rax                    
         movq    $2, %rdi                   
-        popq    %rdx
         popq    %rsi
+        movl    (%rsi), %edx                # get string length
+        lea     4(%rsi), %rsi               # get address of string
         syscall                             # print sled specific info
         movq    $1, %rax                    
         movq    $2, %rdi                   
