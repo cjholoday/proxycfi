523a87af835905feac15efa91b366670ef6a5ca2
Colton Holoday <choloday@umich.edu>
2017-07-13 09:42:43 -0400

    Add more trickery to typedef test

--- tests/gcc_modifications/typedef/main.c
+++ tests/gcc_modifications/typedef/main.c
@@ -5,6 +5,7 @@ typedef struct {
     char *str;
 } custom_type;
 typedef custom_type custom_type_t;
+typedef custom_type_t custome_type_t_t;
 
 
 void foo(custom_type t) {
@@ -19,11 +20,15 @@ int main() {
     custom_type x;
     void (*fptr1)(custom_type_t) = foo;
     void (*fptr2)(custom_type) = bar;
+    void (*fptr3)(custom_type_t_t) = foo;
 
     x.str = "Hello ";
     fptr1(x);
 
-    x.str = "World!\n";
+    x.str = "World!";
     fptr2(x);
+
+    x.str = "\n";
+    fptr3(x);
 }
 
