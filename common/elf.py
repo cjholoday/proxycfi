8b0c235af20c972158e85addad3136a285641a31
Colton Holoday <choloday@umich.edu>
2017-08-11 10:08:51 -0400

    Add functionality to Elf64 class. Define other elf types

--- common/elf.py
+++ common/elf.py
@@ -1,5 +1,37 @@
 from eprint import eprint
 import struct
+import os
+import gcc_wrappers.spec
+
+class Symbol:
+    def __init__(self, buf = None):
+        if buf:
+            (self.st_name,
+                    self.st_info,
+                    self.st_other,
+                    self.st_shndx,
+                    self.st_value,
+                    self.st_size) = struct.unpack('<IBBHQQ', buf)
+        else:
+            self.st_name = None
+            self.st_other = None
+            self.st_shndx = None
+            self.st_value = None
+            self.st_size = None
+
+class SectionHeader:
+    def __init__(self, buf):
+        (self.sh_name,
+                self.sh_type,
+                self.sh_flags,
+                self.sh_addr,
+                self.sh_offset,
+                self.sh_size,
+                self.sh_link,
+                self.sh_info,
+                self.sh_addralign,
+                self.sh_entsize) = struct.unpack('<IIQQQQIIQQ', buf)
+
 
 class Elf64:
     class Fixup:
@@ -11,11 +43,22 @@ class Elf64:
         self.path = path
         self.error_callback = error_callback
 
-    def section_offset(self, sect_name):
-        if hasattr(self, 'sect_offsets'):
-            return self.sect_offsets[sect_name]
+    class MissingSection(Exception):
+        pass
 
-        self.sect_offsets = dict()
+    def find_section(self, wanted_sect):
+        """Returns a section header which as 'sect_name'
+        
+        raises Elf64.MissingSection if the section cannot be found
+        """
+        print "attempting to find '{}'".format(wanted_sect)
+        if hasattr(self, 'sect_headers'):
+            try:
+                return self.sect_headers[wanted_sect]
+            except KeyError:
+                raise Elf64.MissingSection
+
+        self.sect_headers = dict()
         with open(self.path, 'rb') as elf:
             elf_magic = elf.read(4)
             if elf_magic != '\x7f\x45\x4c\x46':
@@ -24,8 +67,8 @@ class Elf64:
 
             # Get the section header table offset
             elf.seek(16 + 2 + 2 + 4 + 8 + 8)
-            sect_header_offset = struct.unpack('<Q', elf.read(8))[0]
-            if sect_header_offset == 0:
+            sh_table_offset = struct.unpack('<Q', elf.read(8))[0]
+            if sh_table_offset == 0:
                 self.error_callback("elf file '{}' is missing its section header table"
                         .format(self.path))
 
@@ -41,7 +84,7 @@ class Elf64:
             e_shstrndx = struct.unpack('<H', elf.read(2))[0]
 
             # load the section header table string table
-            elf.seek(sect_header_offset + e_shstrndx * 64)
+            elf.seek(sh_table_offset + e_shstrndx * 64)
             shstrtab_offset = struct.unpack('<IIQQQ', elf.read(4 * 2 + 8 * 3))[4]
             shstrtab_size = struct.unpack('<Q', elf.read(8))[0]
 
@@ -49,18 +92,122 @@ class Elf64:
             elf.seek(shstrtab_offset)
             shstrtab = elf.read(shstrtab_size)
 
-            # now build a dictionary of section header offsets
-            elf.seek(sect_header_offset)
+            # now build a dictionary of section headers
+            elf.seek(sh_table_offset)
             for idx in xrange(num_sect_headers):
-                sh_name = struct.unpack('<I', elf.read(4))[0]
-                sh_offset = struct.unpack('<IQQQ', elf.read(4 + 8 * 3))[3]
-                self.sect_offsets[strtab_cstring(shstrtab, sh_name)] = sh_offset
-                elf.seek(sect_header_offset + idx * 64)
+                read = elf.read(64)
+                sect_header = SectionHeader(read)
+                sect_name = strtab_cstring(shstrtab, sect_header.sh_name)
+                self.sect_headers[sect_name] = sect_header
 
-        return self.sect_offsets[sect_name]
+        # self.sect_headers is set up. Call ourselves to finish the job
+        return self.find_section(wanted_sect)
+
+    def init_strtab(self, strtab_name):
+        """Sets self.[strtab_name] if not already initialized"""
+        if hasattr(self, strtab_name):
+            return
+
+        with open(self.path, 'rb') as elf:
+            sh = self.find_section(strtab_name)
+            elf.seek(sh.sh_offset)
+            setattr(self, strtab_name[1:], elf.read(sh.sh_size))
+
+    def get_symbols(self, symtab_name):
+        if symtab_name == '.symtab':
+            self.init_strtab('.strtab')
+        elif symtab_name == '.dynsym':
+            self.init_strtab('.dynstr')
+
+        with open(self.path, 'rb') as elf:
+            symtab_header = self.find_section(symtab_name)
+            elf.seek(symtab_header.sh_offset)
+
+            num_symtab_entries = symtab_header.sh_size / 24
+            for idx in xrange(num_symtab_entries):
+                sym = Symbol(elf.read(24))
+                sym.idx = idx
+                yield sym
+
+    def get_rela_relocs(self, sect_name):
+        sh = self.find_section(sect_name)
+        num_entries = sh.sh_size / 24
+
+        relocs = []
+        with open(self.path, 'rb') as elf:
+            elf.seek(sh.sh_offset)
+            for idx in xrange(num_entries):
+                relocs.append(Rela(elf.read(24)))
+        return relocs
+
+    def get_needed_sls(self):
+        print 'DYNSTR'
+        self.init_strtab('.dynstr')
+
+        print 'DYNAMIC'
+        dyn_sh = self.find_section('.dynamic')
+
+        sonames = []
+        with open(self.path, 'rb') as elf:
+            num_dyn_entries = dyn_sh.sh_size / 16
+            for idx in xrange(num_dyn_entries):
+                elf.seek(dyn_sh.sh_offset + idx * 16)
+                d_tag = struct.unpack('<q', elf.read(8))[0]
+                if d_tag == 1:
+                    d_val = struct.unpack('<Q', elf.read(8))[0]
+                    sonames.append(strtab_cstring(self.dynstr, d_val))
+        return sonames
+
+    def get_deps(self, lspec):
+        """Return a list of Elf64 objects on which elf_path depends
+        
+        lspec is a linker spec. See cdi/gcc_wrappers/spec.py
+        """
+        return self.get_deps_helper(lspec, None)[1:]
+
+    def get_deps_helper(self, lspec, elf_path, paths_seen = None):
+        if paths_seen == None:
+            paths_seen = dict()
+
+        elf = None
+        if elf_path == None:
+            elf = self
+            elf_path = self.path
+        else:
+            elf = Elf64(elf_path, self.error_callback)
+        elfs = [elf]
+
+        for sl_name in elf.get_needed_sls():
+            sl_path = ''
+            if os.path.isabs(sl_name):
+                sl_path = sl_name
+            else:
+                # we're given a SONAME. Get the path
+                sl_path = gcc_wrappers.spec.find_lib(sl_name, lspec)
+            sl_realpath = os.path.realpath(sl_path)
+
+            try:
+                if paths_seen[sl_realpath]:
+                    continue
+            except KeyError:
+                paths_seen[sl_realpath] = True
+                if gcc_wrappers.lib_utils.sl_linker_name(sl_name) == 'libc.so':
+                    # End recursion at libc.so so that ld.so is not added to the 
+                    # dependency list. For now, ld.so will remain non-CDI since it will
+                    # only be used at load time. dlopen and friends are forbidden for
+                    # CDI code
+                    elfs.append(Elf64(sl_realpath, self.error_callback))
+                    continue
+                elfs += self.get_deps_helper(lspec, sl_realpath, paths_seen)
+        return elfs
 
     def fixup(self, elf_fixups):
         pass
+
+
+class Rela:
+    def __init__(self, buf):
+        self.r_offset, self.r_info, self.r_addend = struct.unpack('<QQq', buf)
 
 
 def strtab_cstring(strtab, idx):
