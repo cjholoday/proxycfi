20db7ac32314b933d43406bf637ef3f37eb51f75
Colton Holoday <choloday@umich.edu>
2017-08-22 14:09:26 -0400

    Improve multtab test

--- tests/sl_basic/multtab/main.c
+++ tests/sl_basic/multtab/main.c
@@ -1,4 +1,5 @@
 #include "sl1.h"
+void sl2_call_sl1_foo();
 
 int main() {
     sl1_foo();
@@ -6,5 +7,7 @@ int main() {
     sl1_qux();
 
     sl2_call_sl1_foo();
+    sl1_qux();
+    sl1_qux();
 }
 
