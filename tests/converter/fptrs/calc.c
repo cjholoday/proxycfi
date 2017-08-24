d0058538360b43fa6ccdaa235f54eef35e07d2de
Colton Holoday <choloday@umich.edu>
2017-08-24 11:48:48 -0400

    Add basic function pointer test

---
+++ tests/converter/fptrs/calc.c
@@ -0,0 +1,18 @@
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int subtract(int x, int y) {
+    return x - y;
+}
+
+
+int multiply(int x, int y) {
+    return x * y;
+}
+
+int divide(int x, int y) {
+    return x / y;
+}
+
