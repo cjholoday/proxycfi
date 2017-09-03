7769b058f72415c97553ff5dac2d2bab064487ca
Colton Holoday <choloday@umich.edu>
2017-09-03 12:25:34 -0400

    Fix test to use cdi-gcc sl generation

--- tests/sl_fptrs/callback/run_test.bash
+++ tests/sl_fptrs/callback/run_test.bash
@@ -6,14 +6,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # purpose: generate an unsafe (non-CDI) shared library and link with it
 
-cdi_flags="-g --save-temps -fno-jump-tables"
-tools_dir="$SCRIPT_DIR/../../../tools"
-gen_sl="$tools_dir/gen_sl.bash"
+cdi-gcc --make-sl=libcaller.so caller.c
 
-"$gen_sl" libcaller.so caller.c
 
-cdi-gcc $cdi_flags main.c -L. libcaller.so -o out \
-    -Wl,-rpath="$SCRIPT_DIR" 
+cdi-gcc --use-sl=libcaller.so main.c -o out
 
 if [ "$?" != 0 ]; then
     echo ERROR: Compilation failed!
