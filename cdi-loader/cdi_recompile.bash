cecdc48c0e39351c9b3df27d7bb9915aaf9321c0
Misiker <misiker@umich.edu>
2017-09-05 20:10:38 -0400

    linker script modifications

--- cdi-loader/cdi_recompile.bash
+++ cdi-loader/cdi_recompile.bash
@@ -9,6 +9,10 @@ ln -sf "$(pwd)"/dl-map-segments.h "$(pwd)"/glibc-2.23/elf/dl-map-segments.h
 ln -sf "$(pwd)"/dl-load.c "$(pwd)"/glibc-2.23/elf/dl-load.c
 ln -sf "$(pwd)"/dl-lookup.c "$(pwd)"/glibc-2.23/elf/dl-lookup.c
 ln -sf "$(pwd)"/dl-cdi-fptr.c "$(pwd)"/glibc-2.23/elf/dl-cdi-fptr.c
+ln -sf "$(pwd)"/dl-machine.h "$(pwd)"/glibc-2.23/sysdeps/x86_64/dl-machine.h
+
+touch dl-cdi.c dl-cdi.h rtld.c dl-map-segments.h dl-load.c dl-lookup.c dl-cdi-fptr.c dl-machine.c
+
 
 # we can't symbolic link this one because Make will complain about symlinks
 cp Makefile glibc-2.23/elf/Makefile
