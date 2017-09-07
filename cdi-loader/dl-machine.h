518c8b402ebe6f5b1014d56d8f56f116b8a2cb6c
Colton Holoday <choloday@umich.edu>
2017-09-07 11:37:54 -0400

    Refactor relocation code

--- cdi-loader/dl-machine.h
+++ cdi-loader/dl-machine.h
@@ -507,63 +507,68 @@ elf_machine_rela (struct link_map *map, const ElfW(Rela) *reloc,
 # endif
 	}
     }
-	_dl_debug_printf ("elf_machine_rela e --> reloc_addr = 0x%lx, *reloc_addr = 0x%lx \n", (unsigned long int)reloc_addr, (((unsigned long int) *reloc_addr)));
-	/* _CDI_: Modify the plt to a direct call to the actural relocated address. */
+  _dl_debug_printf ("elf_machine_rela e --> reloc_addr = 0x%lx, *reloc_addr = 0x%lx \n", (unsigned long int)reloc_addr, (((unsigned long int) *reloc_addr)));
+  /* _CDI_: Modify the plt to a direct call to the actural relocated address. */
 
-	if ((unsigned long int)pre_relocation_value > 0x400000){
-		/*Check if the library is cdi compliant library.*/
-		CLB *clb = _cdi_clb_from_soname(map->l_name);
-		// bool is_share_lib = strcmp(map->l_name,'');
-		if (clb || !strcmp(map->l_name,"")){
-			_dl_debug_printf ("**** CDI lib relocation :\n");
-			unsigned char *relocation_addr;
-			/*.plt.got since .got comes after .plt.got negative offset implies .plt.got */
-			if ((unsigned long int)pre_relocation_value >  0xf000000000000000){
-				_dl_debug_printf ("****fixing a slow_plt ****:\n");
-				_dl_debug_printf ("pre_relocation_value = 0x%lx reloc_addr = 0x%lx\n", (unsigned long int)pre_relocation_value, (unsigned long int)reloc_addr);
-				_dl_debug_printf ("relocated_to = %lx \n", (unsigned long int) *reloc_addr);
-				relocation_addr = (unsigned char*)((unsigned long int)pre_relocation_value + (unsigned long int)reloc_addr);
-			}
-			else{
-				if((unsigned long int)reloc_addr > 0x700000000000 && strcmp(map->l_name,"")){		
-					relocation_addr = (unsigned char*)(pre_relocation_value - 6) + map->l_addr;
-				}
-				else{
-					relocation_addr = (unsigned char*)(pre_relocation_value - 6);
-				}
-			}
-			unsigned long int relocated_to = (unsigned long int) *reloc_addr;
+  if ((unsigned long int)pre_relocation_value > 0x400000) {
+      /*Check if the library is cdi compliant library.*/
+      CLB *clb = _cdi_clb_from_soname(map->l_name);
+      if (!clb && strcmp(map->l_name,"")){
+          return;
+      }
+      _dl_debug_printf ("**** CDI lib relocation :\n");
+      unsigned char *relocation_addr;
+      /*.plt.got since .got comes after .plt.got negative offset implies .plt.got */
+      if ((unsigned long int)pre_relocation_value >  0xf000000000000000){
+          _dl_debug_printf ("****fixing a slow_plt ****:\n");
+          _dl_debug_printf ("pre_relocation_value = 0x%lx reloc_addr = 0x%lx\n", (unsigned long int)pre_relocation_value, (unsigned long int)reloc_addr);
+          _dl_debug_printf ("relocated_to = %lx \n", (unsigned long int) *reloc_addr);
+          relocation_addr = (unsigned char*)((unsigned long int)pre_relocation_value + (unsigned long int)reloc_addr);
+      }
+      else{
+          if((unsigned long int)reloc_addr > 0x700000000000 && strcmp(map->l_name,"")){		
+              relocation_addr = (unsigned char*)(pre_relocation_value - 6) + map->l_addr;
+          }
+          else{
+              relocation_addr = (unsigned char*)(pre_relocation_value - 6);
+          }
+      }
+      unsigned long int relocated_to = (unsigned long int) *reloc_addr;
 
-                        if (relocated_to) {
-                            int target_goes_to_cdi_sl = 0;
-                            for (int i = 0; i < _clb_tablen; i++) {
-                                if (relocated_to >= _clb_table[i].l->l_map_start
-                                        && relocated_to < _clb_table[i].l->l_map_end) {
-                                    target_goes_to_cdi_sl = 1;
-                                    break;
-                                }
-                            }
-                            if (target_goes_to_cdi_sl) {
-                                _dl_debug_printf ("***doing fixup\n");
-                                mprotect((void*)((long unsigned int)relocation_addr & ~(GLRO(dl_pagesize) - 1)) , 16, PROT_READ | PROT_WRITE);
-                                
-                                // mov r11, actual_addr
-                                *(relocation_addr) = 0x49;
-                                *(relocation_addr + 1) = 0xbb;
-                                
-                                for(int i = 2; i < 10 ;i++){
-                                        *(relocation_addr + i) = relocated_to >> ((i - 2) * 8) & 0xff;
-                                }
-                                    
-                                    /* Call *%r11 */
-                                *(relocation_addr + 10) = 0x41;
-                                *(relocation_addr + 11) = 0xff;
-                                *(relocation_addr + 12) = 0xd3;
-                                mprotect((void*)((long unsigned int)relocation_addr & ~(GLRO(dl_pagesize) - 1)) , 16, PROT_READ | PROT_EXEC);
-                            }
-			}
-		}
-	}
+      if (!relocated_to) {
+          return;
+      }
+
+      int target_goes_to_cdi_sl = 0;
+      for (int i = 0; i < _clb_tablen; i++) {
+          if (relocated_to >= _clb_table[i].l->l_map_start
+                  && relocated_to < _clb_table[i].l->l_map_end) {
+              target_goes_to_cdi_sl = 1;
+              break;
+          }
+      }
+      if (!target_goes_to_cdi_sl) {
+          return;
+      }
+      _dl_debug_printf ("***doing fixup\n");
+      mprotect((void*)((long unsigned int)relocation_addr & ~(GLRO(dl_pagesize) - 1)),
+              16, PROT_READ | PROT_WRITE);
+
+      // mov r11, actual_addr
+      *(relocation_addr) = 0x49;
+      *(relocation_addr + 1) = 0xbb;
+
+      for(int i = 2; i < 10 ;i++){
+          *(relocation_addr + i) = relocated_to >> ((i - 2) * 8) & 0xff;
+      }
+
+      /* Call *%r11 */
+      *(relocation_addr + 10) = 0x41;
+      *(relocation_addr + 11) = 0xff;
+      *(relocation_addr + 12) = 0xd3;
+      mprotect((void*)((long unsigned int)relocation_addr & ~(GLRO(dl_pagesize) - 1)),
+              16, PROT_READ | PROT_EXEC);
+  }
 }
 
 auto inline void
