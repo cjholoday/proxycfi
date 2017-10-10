20486fbfbd6fcbb009f3ad1dc723320e73ae9992
Colton Holoday <choloday@umich.edu>
2017-10-10 13:12:49 -0400

    Add stress test for many many fp sleds

---
+++ tests/sl_fptrs/many_fp_sleds/run_test.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clean
+
+cdi-gcc --make-sl=libtargetful1.so.1.0.0 targetful1.c
+check "compilation of libtargetful1.so failed" || exit 1
+
+cdi-gcc --make-sl=libtargetful2.so.1.0.0 targetful2.c
+check "compilation of libtargetful2.so failed" || exit 1
+
+cdi-gcc --use-sl=libtargetful1.so,libtargetful2.so main.c -o out
+check "compilation of main.c failed" || exit 1
+
+./out > output
+check "./out exited with error" || exit 1
+
+diff output correct_output
+check "incorrect output" || exit 1
