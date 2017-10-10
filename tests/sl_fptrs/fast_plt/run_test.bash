5eda030734df5d223fb3666ef232103b0869fc1f
Colton Holoday <choloday@umich.edu>
2017-10-10 13:11:22 -0400

    Clean up fast plt test

--- tests/sl_fptrs/fast_plt/run_test.bash
+++ tests/sl_fptrs/fast_plt/run_test.bash
@@ -1,34 +1,19 @@
 #!/bin/bash
 
-rm -f *.o *.s *.json *.i *.ftypes *.out *.fptypes output out
-
-# remove versioned shared libraries, symlinks and all
-rm -f *.so *.so.[0-9]*  *.so.[0-9]*.[0-9]*
-
-verify="../../../verifier/verify.py"
+clean
 
 cdi-gcc --make-sl=libadd.so.1.0.0 add.c
-if [ "$?" != 0 ]; then
-    echo ERROR: Compilation failed!
-    exit 1
-fi
+check "compilation of libadd.so failed" || exit 1
 
 cdi-gcc --make-sl=libdo_add.so.2.0.0 --use-sl=libadd.so do_add.c
-if [ "$?" != 0 ]; then
-    echo ERROR: Compilation failed!
-    exit 1
-fi
+check "compilation of libdo_add.so failed" || exit 1
 
 cdi-gcc --use-sl=libadd.so.1,libdo_add.so main.c -o out
-if [ "$?" != 0 ]; then
-    echo ERROR: Compilation failed!
-    exit 1
-fi
+check "compilation of main.c failed" || exit 1
 
 # Check that the output is correct
 ./out > output
+check "./out exited with error" || exit 1
+
 diff output correct_output
-if [ "$?" != 0 ]; then
-    echo ERROR: Incorrect output!
-    exit 1
-fi
+check "incorrect output" || exit 1
